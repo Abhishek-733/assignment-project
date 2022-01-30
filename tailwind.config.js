ca48652a3765aa5d10f0c6d314e39cb67de95e3b
abhishek <abhishekkrsingh095@gmail.com>
2022-01-30 19:57:40 +0530

    aaded nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./src/**/*.{html,css}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
+      colors: {
+        clifford: '#da373d',
+        lightblue: '#1A75FC',
+        lightbackground: '#D5E6FF',
+        deepblue: '#0D3B7E'
+      }
     }
   },
   plugins: [],
